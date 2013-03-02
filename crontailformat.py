959b9b52ed9157fc0d50dae67189b841ae37ff0c
Ali Clark <ali@clark.gb.net>
2013-03-02 13:40:36 +0000

    fix to zero pad day of month numbers if not already zero-padded

--- crontailformat.py
+++ crontailformat.py
@@ -7,8 +7,18 @@ import re
 import datetime
 import time
 
+bad_dom = re.compile(r'([a-zA-Z]{3})(, )([1-9])( )([a-zA-Z]{3})(.*)')
+
 # 'Sun, 17 Feb 2013 17:42:28 +0000 (UTC)'
-def tomillistamp(ds):
+def tomillistamp(ids):
+    ds = ids
+
+    # strptime only accepts 2 digit day-of-month values, so if we have
+    # a single digit value, zero pad it to 2 digits first.
+    res = bad_dom.match(ids)
+    if res:
+        ds = res.group(1) + res.group(2) + '0' + res.group(3) + res.group(4) + res.group(5) + res.group(6)
+
     secshift = ((int(ds[26] + ds[27:29]) * 60) + int(ds[26] + ds[29:31])) * 60
     dt = datetime.datetime.strptime(ds[:25], '%a, %d %b %Y %H:%M:%S')
     return str(int(round(time.mktime(dt.timetuple())) - secshift) * 1000)
